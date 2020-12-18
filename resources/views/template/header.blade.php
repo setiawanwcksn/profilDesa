1f1c3e51ac37e4d79d9fbdb5f0f3786d83eb6ffa
azamfullbuster <azamfullbuster@gmail.com>
2020-12-18 21:36:27 +0700

    Edit template

--- resources/views/template/header.blade.php
+++ resources/views/template/header.blade.php
@@ -1,133 +1,64 @@
-<!DOCTYPE html>
-
-<head>
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<title>Listing &mdash; 100% Free Fully Responsive HTML5 Template by FREEHTML5.co</title>
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<meta name="description" content="Free HTML5 Template by FREEHTML5.CO" />
-	<meta name="keywords" content="free html5, free template, free bootstrap, html5, css3, mobile first, responsive" />
-	<meta name="author" content="FREEHTML5.CO" />
-
-
-	<meta property="og:title" content="" />
-	<meta property="og:image" content="" />
-	<meta property="og:url" content="" />
-	<meta property="og:site_name" content="" />
-	<meta property="og:description" content="" />
-	<meta name="twitter:title" content="" />
-	<meta name="twitter:image" content="" />
-	<meta name="twitter:url" content="" />
-	<meta name="twitter:card" content="" />
-	<!-- untuk font awesome -->
-
-	<!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
-	<link rel="shortcut icon" href="{{asset('favicon.ico')}}">
-
-	<!-- Add glyphicon -->
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
-
-	<link href='https://fonts.googleapis.com/css?family=Open+Sans:400,700,300' rel='stylesheet' type='text/css'>
-	<!-- Animate.css -->
-	<link rel="stylesheet" href="{{ asset('css/animate.css') }}">
-	<!-- Icomoon Icon Fonts-->
-	<link rel="stylesheet" href="{{ asset('css/icomoon.css') }}">
-	<!-- Bootstrap  -->
-	<link rel="stylesheet" href="{{ asset('css/bootstrap.css') }}">
-	<!-- Superfish -->
-	<link rel="stylesheet" href="{{ asset('/css/superfish.css') }}">
-
-	<link rel="stylesheet" href="{{ asset('css/style.css') }}">
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css"
-		integrity="sha512-1PKOgIY59xJ8Co8+NE6FZ+LOAZKjy+KY8iq0G4B3CyeY6wYHN3yt9PW0XpSriVlkMXe40PTKnXrLnZ9+fkDaog=="
-		crossorigin="anonymous" />
-
-	<!-- Styles -->
-	<!-- Bootstrap CSS -->
-	<link href="{{ asset('css/bootstrap.min.css')}}" rel="stylesheet">
-	<!-- Animate CSS -->
-	<link href="{{ asset('css/animate.min.css')}}" rel="stylesheet">
-	<!-- Basic stylesheet -->
-	<link rel="stylesheet" href="{{ asset('css/owl.carousel.css')}}">
-	<!-- Custom CSS -->
-	<link href="{{ asset('css/style-color.css')}}" rel="stylesheet">
-
-
-	<!-- Modernizr JS -->
-	<script src="{{ asset('js/modernizr-2.6.2.min.js') }}"></script>
-	<!-- FOR IE9 below -->
-	<!--[if lt IE 9]>
-	<script src="js/respond.min.js"></script>
-	<![endif]-->
-
-</head>
-
-<body>
-	<div id="fh5co-wrapper">
-		<div id="fh5co-page" style="background-color: black">
-			<div id="fh5co-header">
-				<header id="fh5co-header-section">
-					<div class="container">
-						<div class="nav-header">
-							<a href="#" class="js-fh5co-nav-toggle fh5co-nav-toggle"><i></i></a>
-							<h1 id="fh5co-logo"><a href="index.html">Pathur</a></h1>
-							<!-- START #fh5co-menu-wrap -->
-							<nav id="fh5co-menu-wrap" role="navigation">
-								<ul class="sf-menu" id="fh5co-primary-menu">
-									<li class="active">
-										<a href="index.html">Home</a>
-									</li>
-									<li>
-										<a href="#" class="fh5co-sub-down sf-with-ul">Listing</a>
-										<ul class="fh5co-sub-menu ">
-											<li><a href="http://freehtml5.co/preview/?item=build-free-html5-bootstrap-template"
-													target="_blank">Build</a></li>
-											<li><a href="http://freehtml5.co/preview/?item=work-free-html5-template-bootstrap"
-													target="_blank">Work</a></li>
-											<li><a href="http://freehtml5.co/preview/?item=light-free-html5-template-bootstrap"
-													target="_blank">Light</a></li>
-											<li><a href="http://freehtml5.co/preview/?item=relic-free-html5-template-using-bootstrap"
-													target="_blank">Relic</a></li>
-											<li><a href="http://freehtml5.co/preview/?item=display-free-html5-template-using-bootstrap"
-													target="_blank">Display</a></li>
-											<li><a href="http://freehtml5.co/preview/?item=sprint-free-html5-template-bootstrap"
-													target="_blank">Sprint</a></li>
-										</ul>
-									</li>
-									<li>
-										<a href="#" class="fh5co-sub-ddown sf-with-ul">Joko Fathur</a>
-										<ul class="fh5co-sub-menu">
-											<li><a href="left-sidebar.html">Web Development</a></li>
-											<li><a href="right-sidebar.html">Branding &amp; Identity</a></li>
-											<li>
-												<a href="#" class="fh5co-sub-ddown">Free HTML5</a>
-												<ul class="fh5co-sub-menu">
-													<li><a href="http://freehtml5.co/preview/?item=build-free-html5-bootstrap-template"
-															target="_blank">Build</a></li>
-													<li><a href="http://freehtml5.co/preview/?item=work-free-html5-template-bootstrap"
-															target="_blank">Work</a></li>
-													<li><a href="http://freehtml5.co/preview/?item=light-free-html5-template-bootstrap"
-															target="_blank">Light</a></li>
-													<li><a href="http://freehtml5.co/preview/?item=relic-free-html5-template-using-bootstrap"
-															target="_blank">Relic</a></li>
-													<li><a href="http://freehtml5.co/preview/?item=display-free-html5-template-using-bootstrap"
-															target="_blank">Display</a></li>
-													<li><a href="http://freehtml5.co/preview/?item=sprint-free-html5-template-bootstrap"
-															target="_blank">Sprint</a></li>
-												</ul>
-											</li>
-											<li><a href="#">UI Animation</a></li>
-											<li><a href="#">Copywriting</a></li>
-											<li><a href="#">Photography</a></li>
-										</ul>
-									</li>
-									<li><a href="contact.html">Contact</a></li>
-								</ul>
-							</nav>
-						</div>
-					</div>
-				</header>
-
+<div id="fh5co-header">
+	<header id="fh5co-header-section">
+		<div class="container">
+			<div class="nav-header">
+				<a href="#" class="js-fh5co-nav-toggle fh5co-nav-toggle"><i></i></a>
+				<h1 id="fh5co-logo"><a href="index.html">Pathur</a></h1>
+				<!-- START #fh5co-menu-wrap -->
+				<nav id="fh5co-menu-wrap" role="navigation">
+					<ul class="sf-menu" id="fh5co-primary-menu">
+						<li class="active">
+							<a href="index.html">Home</a>
+						</li>
+						<li>
+							<a href="#" class="fh5co-sub-down sf-with-ul">Listing</a>
+							<ul class="fh5co-sub-menu ">
+								<li><a href="http://freehtml5.co/preview/?item=build-free-html5-bootstrap-template"
+										target="_blank">Build</a></li>
+								<li><a href="http://freehtml5.co/preview/?item=work-free-html5-template-bootstrap"
+										target="_blank">Work</a></li>
+								<li><a href="http://freehtml5.co/preview/?item=light-free-html5-template-bootstrap"
+										target="_blank">Light</a></li>
+								<li><a href="http://freehtml5.co/preview/?item=relic-free-html5-template-using-bootstrap"
+										target="_blank">Relic</a></li>
+								<li><a href="http://freehtml5.co/preview/?item=display-free-html5-template-using-bootstrap"
+										target="_blank">Display</a></li>
+								<li><a href="http://freehtml5.co/preview/?item=sprint-free-html5-template-bootstrap"
+										target="_blank">Sprint</a></li>
+							</ul>
+						</li>
+						<li>
+							<a href="#" class="fh5co-sub-ddown sf-with-ul">Joko Fathur</a>
+							<ul class="fh5co-sub-menu">
+								<li><a href="left-sidebar.html">Web Development</a></li>
+								<li><a href="right-sidebar.html">Branding &amp; Identity</a></li>
+								<li>
+									<a href="#" class="fh5co-sub-ddown">Free HTML5</a>
+									<ul class="fh5co-sub-menu">
+										<li><a href="http://freehtml5.co/preview/?item=build-free-html5-bootstrap-template"
+												target="_blank">Build</a></li>
+										<li><a href="http://freehtml5.co/preview/?item=work-free-html5-template-bootstrap"
+												target="_blank">Work</a></li>
+										<li><a href="http://freehtml5.co/preview/?item=light-free-html5-template-bootstrap"
+												target="_blank">Light</a></li>
+										<li><a href="http://freehtml5.co/preview/?item=relic-free-html5-template-using-bootstrap"
+												target="_blank">Relic</a></li>
+										<li><a href="http://freehtml5.co/preview/?item=display-free-html5-template-using-bootstrap"
+												target="_blank">Display</a></li>
+										<li><a href="http://freehtml5.co/preview/?item=sprint-free-html5-template-bootstrap"
+												target="_blank">Sprint</a></li>
+									</ul>
+								</li>
+								<li><a href="#">UI Animation</a></li>
+								<li><a href="#">Copywriting</a></li>
+								<li><a href="#">Photography</a></li>
+							</ul>
+						</li>
+						<li><a href="contact.html">Contact</a></li>
+					</ul>
+				</nav>
 			</div>
-			@yield('content')
-			@extends('template/footer')
+		</div>
+	</header>
+
+</div>
